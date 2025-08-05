1793465b9e4512a17162644bb1fdfd30408a677f
Amith S <amith.s@anorasolutions.com>
2025-08-05 12:06:45 +0530

    First change

---
+++ A-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+\\this is the first change in A2
+int main(){
+    int arr[20];
+    unsigned int a,i=0;
+    printf("The number is\n");
+    scanf("%d",&a);
+    while(a/2!=0){
+        arr[i]=a%2;
+        a/=2;
+        i++;
+        if(a==1){
+            arr[i]=1;
+        }
+    }
+    int j=i;
+    
+    while(j>=0)
+    {
+        printf("%d",arr[j]);
+        j--;
+    }
+}This is the first code
---
+++ B-2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+}\\this is the first change in B2
+void check(int a){
+if(a%2==0){
+printf("even");
+}
+else{
+printf("odd");}}
+int main(){
+    int a,b;
+    printf("enter the two numbers\n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int x;
+    x=a;
+    b=a;
+    b=x;
+    printf("a=%d,b=%d",a,b);
+    printf("the number is\n");
+    check(a);
+    return 0;
+}
